0e35efd221108181bb627253137efcc493fd489d
ChoiysApple <chcs1370@gmail.com>
2022-01-05 14:26:05 +0900

    Create DiscoverCollectionView
    Movie poster placeholder asset added

---
+++ MovieApp/MovieApp/Constant.swift
@@ -0,0 +1,13 @@
+//
+//  Constant.swift
+//  MovieApp
+//
+//  Created by Daegeon Choi on 2022/01/04.
+//
+
+import Foundation
+
+
+struct ColorName {
+    static let background_color = "BgColor"
+}
