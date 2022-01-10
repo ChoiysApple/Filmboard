4ddbe2efafb6f8484b8bd77ebb84e72bb8e04a2e
ChoiysApple <chcs1370@gmail.com>
2022-01-10 15:11:17 +0900

    Create Sample Credit Page

--- MovieApp/MovieApp/View/CharView/ChartViewController.swift
+++ MovieApp/MovieApp/View/CharView/ChartViewController.swift
@@ -62,27 +62,3 @@ extension ChartViewController: UITableViewDelegate {
 //    }
 }
 
-#if DEBUG
-import SwiftUI
-struct ViewControllerRepresentable: UIViewControllerRepresentable {
-    
-func updateUIViewController(_ uiView: UIViewController,context: Context) {
-        // leave this empty
-}
-@available(iOS 13.0.0, *)
-func makeUIViewController(context: Context) -> UIViewController{
-    ChartViewController()
-    }
-}
-@available(iOS 13.0, *)
-struct ViewControllerRepresentable_PreviewProvider: PreviewProvider {
-    static var previews: some View {
-        Group {
-            ViewControllerRepresentable()
-                .ignoresSafeArea()
-                .previewDisplayName(/*@START_MENU_TOKEN@*/"Preview"/*@END_MENU_TOKEN@*/)
-                .previewDevice(PreviewDevice(rawValue: "iPhone 11"))
-        }
-        
-    }
-} #endif
