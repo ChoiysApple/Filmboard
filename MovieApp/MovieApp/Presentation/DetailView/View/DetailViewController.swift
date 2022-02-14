8e23ec448f96befa9498e6dbcc8b484991ef4b09
ChoiysApple <chcs1370@gmail.com>
2022-02-14 15:49:49 +0900

    Create DetailViewController.swift

---
+++ MovieApp/MovieApp/Presentation/DetailView/View/DetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.swift
+//  MovieApp
+//
+//  Created by Daegeon Choi on 2022/02/14.
+//
+
+import SnapKit
+import Foundation
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    
+    
+}
