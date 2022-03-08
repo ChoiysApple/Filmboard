8b12519c90337aa6bd6ead7f7caf70104acfe4db
ChoiysApple <chcs1370@gmail.com>
2022-03-08 15:37:35 +0900

    Add line to Liscense Label

--- MovieApp/MovieApp/Presentation/CreditView/View/CreditFootherView.swift
+++ MovieApp/MovieApp/Presentation/CreditView/View/CreditFootherView.swift
@@ -14,17 +14,31 @@ class CreditFootherView: UITableViewHeaderFooterView {
         $0.font = UIFont.systemFont(ofSize: 11, weight: .regular)
         $0.text = "MIT License | Copyright (c) 2022 Daegun Choi"
     }
+    
+    lazy var topDivider = UIView().then {
+        $0.backgroundColor = .gray
+        $0.isHidden = false
+    }
 
 
     override init(reuseIdentifier: String?) {
         super.init(reuseIdentifier: reuseIdentifier)
         
         self.addSubview(label)
+        self.addSubview(topDivider)
         
         label.snp.makeConstraints { make in
             make.centerX.equalToSuperview()
             make.bottom.equalToSuperview()
         }
+        
+        topDivider.snp.makeConstraints { make in
+            make.height.equalTo(0.3)
+            make.centerY.equalTo(label.snp.top).offset(-5)
+            make.left.equalTo(label.snp.left)
+            make.right.equalTo(label.snp.right)
+        }
+        
     }
     
     
