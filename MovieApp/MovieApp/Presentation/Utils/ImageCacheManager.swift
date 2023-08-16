76922f4eee7f9b768fa234d00d19738ca086df9a
ChoiysApple <chcs1370@gmail.com>
2023-08-16 19:48:57 +0900

    feat: add markup comment

--- MovieApp/MovieApp/Presentation/Utils/ImageCacheManager.swift
+++ MovieApp/MovieApp/Presentation/Utils/ImageCacheManager.swift
@@ -9,16 +9,27 @@ import UIKit
 
 /// Singleton for Image Caching
 class ImageCacheManager {
+    
+    /// Storage to save cached UIImage
     private let cache = NSCache<NSString, UIImage>()
     static let shared = ImageCacheManager()
     
-    private init() {}
-    
+    /**
+    Get image from cache data for url String
+    - Parameter key:  String url of UIImage
+    - Returns: Retrun cached image for url. Retrun nil when cached image is not exist
+    */
     func loadCachedData(for key: String) -> UIImage? {
         let itemURL = NSString(string: key)
         return cache.object(forKey: itemURL)
     }
     
+    /**
+    Save UIImage to cache data
+     - Parameters:
+        - image: UIImage to save in cache data
+        - key:  String url of UIImage
+     */
     func saveCacheData(of image: UIImage, for key: String) {
         let itemURL = NSString(string: key)
         cache.setObject(image, forKey: itemURL)
@@ -26,15 +37,20 @@ class ImageCacheManager {
 }
 
 extension UIImageView {
-    
-    /// Set image to UIImageView with Cached Image data or data from URL
-    func setImage(_ urlString: String?) {
+
+    /**
+     Set image to UIImageView with Cached Image data or data from URL
+     - Parameters:
+        - urlString: String url of image
+        - forceOption: Skip getting image from cache data and force to get image from url when true. default false
+     */
+    func setImage(_ urlString: String?, forceOption: Bool = false) {
         
         DispatchQueue.global().async {
             guard let imagePath = urlString else { return }
             
             // Cached Image is available
-            if let cachedData = ImageCacheManager.shared.loadCachedData(for: imagePath) {
+            if let cachedData = ImageCacheManager.shared.loadCachedData(for: imagePath), forceOption == false {
                 DispatchQueue.main.async {
                     self.image = cachedData
                 }
