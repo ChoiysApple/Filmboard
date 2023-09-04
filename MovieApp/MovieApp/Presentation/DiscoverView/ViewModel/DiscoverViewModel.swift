3b64c23821beca055f1f019659d099001809c057
Daegun Choi  <chcs1370@gmail.com>
2023-09-04 22:14:17 +0900

    refactor: refactor DiscoverViewModel
    - Change property naming
    - add disposing

--- MovieApp/MovieApp/Presentation/DiscoverView/ViewModel/DiscoverViewModel.swift
+++ MovieApp/MovieApp/Presentation/DiscoverView/ViewModel/DiscoverViewModel.swift
@@ -11,24 +11,23 @@ import RxRelay
 
 class DiscoverViewModel {
     
-    static let shared = DiscoverViewModel()
-    
-    var movieFrontObservable = BehaviorRelay<[DiscoverCollectionViewSection]>(value: [])
-    let dataSource = DiscoverCollectionViewDataSource.dataSource()
+    var movieListData = BehaviorRelay<[DiscoverCollectionViewSection]>(value: [])
+    private var disposeBag = DisposeBag()
         
     func requestData(page: Int) {
         let url = APIService.configureUrlString(category: .NowPlaying, language: .English, page: page)
-        _ = APIService.fetchWithRx(url: url, retries: 2)
+        APIService.fetchWithRx(url: url, retries: 2)
             .map { data -> [MovieListResult] in
                 
-                let response = try! JSONDecoder().decode(MovieList.self, from: data)
+                guard let response = try? JSONDecoder().decode(MovieList.self, from: data) else { return [] }
                 return response.results
             }.map({ movieList in
                 let items = movieList.map { DiscoverCollectionViewItem(movie: MovieFront.convertFromMovieInfo(movie: $0))}
                 return [DiscoverCollectionViewSection(items: items)]
             })
             .take(1)
-            .bind(to: movieFrontObservable)
+            .bind(to: movieListData)
+            .disposed(by: disposeBag)
     }
     
     func requestData(keyword: String, page: Int) {
@@ -39,7 +38,7 @@ class DiscoverViewModel {
         }
         
         let url = APIService.configureUrlString(keyword: keyword, language: .English, page: page)
-        _ = APIService.fetchWithRx(url: url, retries: 2)
+        APIService.fetchWithRx(url: url, retries: 2)
             .map { data -> [MovieListResult] in
                 
                 let response = try! JSONDecoder().decode(MovieList.self, from: data)
@@ -49,7 +48,8 @@ class DiscoverViewModel {
                 return [DiscoverCollectionViewSection(items: items)]
             })
             .take(1)
-            .bind(to: movieFrontObservable)
+            .bind(to: movieListData)
+            .disposed(by: disposeBag)
     }
     
 
